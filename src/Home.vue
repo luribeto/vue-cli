dee4ff2457cc6ff66eb986148bce31fa40f3e713
Leonardo Uribe <luribeto@gmail.com>
2020-01-14 17:00:59 -0500

    first components

---
+++ src/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <server-status v-for="(server, idx) in 5" v-bind:key="idx"></server-status>
+  </div>
+</template>
+
+<script>
+import ServerStatus from "./ServerStatus.vue";
+
+export default {
+  components: {
+    "server-status": ServerStatus
+  }
+};
+</script>
